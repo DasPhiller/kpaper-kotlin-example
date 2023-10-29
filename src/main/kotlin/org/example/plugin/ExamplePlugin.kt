d96be8d6c6ad8baa5209f6625e353668848fb8e6
DasPhiller <73665072+DasPhiller@users.noreply.github.com>
2023-10-29 12:37:48 +0100

    Update ExamplePlugin.kt

--- src/main/kotlin/org/example/plugin/ExamplePlugin.kt
+++ src/main/kotlin/org/example/plugin/ExamplePlugin.kt
@@ -1,11 +1,14 @@
 package org.example.plugin
 
 import de.miraculixx.kpaper.main.KPaper
+import dev.jorel.commandapi.CommandAPI
+import dev.jorel.commandapi.CommandAPIBukkitConfig
 
 class ExamplePlugin : KPaper() {
 
     override fun startup() {
         //Startup
+        CommandAPI.onLoad(CommandAPIBukkitConfig(this).silentLogs(true))
     }
 
 }
